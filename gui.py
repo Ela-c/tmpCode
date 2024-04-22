0e36da919db5b221f6be27dd6e2e6db850880619
Turtleshot <80540625+Ela-c@users.noreply.github.com>
2024-04-22 10:44:11 +1000

    update program

--- gui.py
+++ gui.py
@@ -16,15 +16,16 @@ myFont = tkfont.Font(family="Helvetica", size=12, weight="bold")
 ## EVENT FUNCTIONS ##
 
 
-def ledToggle(btn: str, pin: int, pressed: bool):
-
-    if pressed:
-        v.set(0)
+def ledToggle(pin: int):
+    print(f"button pressed {sharedVariable.get()}")
     # if led.is_lit:
     #     led.off()
     #     v.set(0)
 
-    return print(f"{btn} button pressed")
+
+def diselectAllBtns():
+    # turn off LEDs
+    sharedVariable.set("")
 
 
 def close():
@@ -35,28 +36,51 @@ def close():
     mainWindow.destroy()
 
 
+availableColors = {
+    'red': {
+        'label': 'Red',
+        'hexCode': "#ff0000",
+        'value': 'red',
+        'pin': 2,
+    }, 'blue': {
+        'label': 'Blue',
+        'hexCode': '#3944bc',
+        'value': 'blue',
+        'pin': 2,
+    },
+    'green': {
+        'label': 'Green',
+        'hexCode': '#3cb043',
+        'value': 'green',
+        'pin': 2,
+    },
+
+}
+
 ### WIDGETS ###
 
-v = tk.IntVar(mainWindow, 1)
+sharedVariable = tk.StringVar(mainWindow, "")
 
-noneBtn = tk.Radiobutton(mainWindow, indicatoron=0, value=0, text="None", font=myFont,
-                         command=lambda: ledToggle(btn="none", pin=-1, pressed=False), bg="gray", width=24)
+noneBtn = tk.Radiobutton(mainWindow, indicatoron=0, variable=sharedVariable, value="", text="None", font=myFont,
+                         command=lambda: diselectAllBtns(), bg="gray", width=24).grid()
 
-redBtn = tk.Radiobutton(mainWindow, indicatoron=0, value=1, text="Red", font=myFont,
-                        command=lambda: ledToggle(btn="red", pin=2, pressed=v.get() == 1), bg="#ff0000", activebackground="#7a1712", width=24)
-blueBtn = tk.Radiobutton(mainWindow, indicatoron=0, text="Blue", value=2, font=myFont,
-                         command=lambda: ledToggle(btn="blue", pin=2, pressed=v.get() == 2), bg="#3944bc", activebackground="#0a1172", width=24)
-greenBtn = tk.Radiobutton(mainWindow, indicatoron=0, text="Green", value=3, font=myFont,
-                          command=lambda: ledToggle(btn="green", pin=2, pressed=v.get() == 3), bg="#3cb043", activebackground="#028a0f", width=24)
+redbtn = tk.Radiobutton(mainWindow, indicatoron=0, variable=sharedVariable, value=availableColors['red']['value'], text=availableColors['red']['label'], font=myFont,
+                        command=lambda: ledToggle(pin=availableColors['red']['pin']), bg=availableColors['red']['hexCode'], width=24).grid()
+
+greeBtn = tk.Radiobutton(mainWindow, indicatoron=0, variable=sharedVariable, value=availableColors['green']['value'], text=availableColors['green']['label'], font=myFont,
+                         command=lambda: ledToggle(pin=availableColors['green']['pin']), bg=availableColors['green']['hexCode'], width=24).grid()
+
+blueBtn = tk.Radiobutton(mainWindow, indicatoron=0, variable=sharedVariable, value=availableColors['blue']['value'], text=availableColors['blue']['label'], font=myFont,
+                         command=lambda: ledToggle(pin=availableColors['blue']['pin']), bg=availableColors['blue']['hexCode'], width=24).grid()
+
+
+# for value in availableColors.values():
+#     tk.Radiobutton(mainWindow, indicatoron=0, value=value['value'], text=value['label'], font=myFont,
+#                    command=lambda: ledToggle(btn=value['value'], pin=2, currValue=sharedVariable.get()), bg=value['hexCode'], width=24).grid()
+
 
 exitBtn = tk.Button(mainWindow, text="Exit", font=myFont,
-                    command=close, bg="gray", width=24)
-
-noneBtn.grid()
-redBtn.grid()
-blueBtn.grid()
-greenBtn.grid()
-exitBtn.grid()
+                    command=close, bg="gray", width=24).grid()
 
 mainWindow.protocol("WM_DELETE_WINDOW", close)
 
